e73daedd71c3b2fe1af5604360f953266acf67bf
Chan Mun Tock <muntock@yahoo.com.my>
2018-01-01 23:09:45 +0800

    Added simple repeating background

--- miao-dentsu-thecnydash/Views/Home/Index.vbhtml
+++ miao-dentsu-thecnydash/Views/Home/Index.vbhtml
@@ -3,5 +3,15 @@
 End Code
 
 <div class="row">
-
+    <div id="gameWrapper"></div>
 </div>
+
+@Section scripts
+    <script src="~/js/phaser.js"></script>
+    <script src="~/js/game.js"></script>
+    <script>
+        $(function () {
+            cnyDash();
+        })
+    </script>
+End Section
---
+++ miao-dentsu-thecnydash/js/game.js
@@ -0,0 +1,26 @@
+﻿var cnyDash = function () {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameWrapper',
+                               { preload: preload, create: create, update: update });
+
+    var bg;
+    var direction = -1;
+
+    function preload() {
+        game.load.image('background', '/assets/background.png');
+    }
+
+    function create() {
+        bg = game.add.tileSprite(0, 0, 1024, 512, 'background');
+
+        game.input.onTap.add(Jump, this);
+    }
+
+    function update() {
+        bg.tilePosition.x += direction;        
+    }
+
+    function Jump(pointer) {
+        console.log(pointer);
+        direction = direction * -1;
+    }
+};
